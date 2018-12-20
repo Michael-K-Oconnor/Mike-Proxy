7e6062fdb9dd1762ca91f942a203ba5aba1e8f6d
Mike O'Connor <m.k.oconnor53@gmail.com>
2018-12-20 12:27:10 -0600

    update commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const bodyParser = require("body-parser");
+const db = require("./db/db.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
