c615e3b1ffe20beed1a5e9beaa9c5b6d1b7aafff
Mike O'Connor <m.k.oconnor53@gmail.com>
2018-12-21 11:11:19 -0600

    Adds additional routes

--- server.js
+++ server.js
@@ -11,6 +11,62 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+////////////////////////////////////////////////
+//////////////// Comments Routes ////////////////
+////////////////////////////////////////////////
+
+app.get("/comments", (req, res) => {
+  axios
+    .get("http://localhost:3001/comments", {
+      params: req.query
+    })
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log("There was an error with GET request to comments server");
+      res.sendStatus(500);
+    });
+});
+
+app.post("/comments", (req, res) => {
+  let options = req.body;
+  axios
+    .post("http://localhost:3001/comments", options)
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log("There was an error with POST request to comments server");
+      res.sendStatus(500);
+    });
+});
+
+////////////////////////////////////////////////
+//////////////// Project Routes ////////////////
+////////////////////////////////////////////////
+
+app.get("/projects", (req, res) => {
+  axios
+    .get("http://localhost:3002/projects", {
+      params: req.query
+    })
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log("There was an error with GET request to projects server");
+      res.sendStatus(500);
+    });
+});
+
+////////////////////////////////////////////////
+//////////////// Pleadge Routes ////////////////
+////////////////////////////////////////////////
+
 app.get("/pledges", (req, res) => {
   axios
     .get("http://localhost:3003/pledges", {
@@ -36,6 +92,27 @@ app.post("/pledges", (req, res) => {
     })
     .catch(err => {
       console.log("There was an error with POST request to pledges server");
+      res.sendStatus(500);
+    });
+});
+
+////////////////////////////////////////////////
+//////////////// Project Routes ////////////////
+////////////////////////////////////////////////
+
+app.get("/relatedProjects", (req, res) => {
+  axios
+    .get("http://localhost:3004/relatedProjects", {
+      params: req.query
+    })
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log(
+        "There was an error with GET request to related projects server"
+      );
       res.sendStatus(500);
     });
 });
