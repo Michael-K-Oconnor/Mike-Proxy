21339ce43b6d83cb42c19022a2a9a3ed3f59613b
Mike O'Connor <m.k.oconnor53@gmail.com>
2018-12-20 21:19:28 -0600

    Adds post route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const bodyParser = require("body-parser");
-const db = require("./db/db.js");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +10,35 @@ const port = process.env.PORT || 3000;
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/pledges", (req, res) => {
+  axios
+    .get("http://localhost:3003/pledges", {
+      params: req.query
+    })
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log("There was an error with GET request to pledges server");
+      res.sendStatus(500);
+    });
+});
+
+app.post("/pledges", (req, res) => {
+  let options = req.body;
+  axios
+    .post("http://localhost:3003/pledges", options)
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log("There was an error with POST request to pledges server");
+      res.sendStatus(500);
+    });
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
