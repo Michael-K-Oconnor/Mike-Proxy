afd9a008d283db42c8e4ce91462bb087bf67a5d7
Mike O'Connor <m.k.oconnor53@gmail.com>
2018-12-27 17:21:15 -0600

    Start refactor for EC2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: "Proxy",
+      script: "./server/server.js",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "ec2-18-234-245-254.compute-1.amazonaws.com",
+      port: "22",
+      key: "~/.ssh/firstInstance.pem",
+      ref: "origin/master",
+      repo: "git@github.com:Michael-K-Oconnor/Mike-Proxy.git",
+      path: "/home/ubuntu/Mike-Proxy",
+      "post-deploy":
+        "npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production"
+    }
+  }
+};
---
+++ server/app.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "../public")));
+
+/////  CONFIG ROUTES FROM ENV  ///////////////
+const [commentsRoute, projectsRoute, pledgesRoute, relatedRoute] = [
+  process.env.COMMENTS_HOST + ":" + process.env.COMMENTS_PORT,
+  process.env.PROJECTS_HOST + ":" + process.env.PROJECTS_PORT,
+  process.env.PLEDGES_HOST + ":" + process.env.PLEDGES_PORT,
+  process.env.RELATED_HOST + ":" + process.env.RELATED_PORT
+];
+
+////////////////////////////////////////////////
+//////////////// Comments Routes ////////////////
+////////////////////////////////////////////////
+
+app.get("/comments", (req, res) => {
+  axios
+    .get(commentsRoute + "/comments", {
+      params: req.query
+    })
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log("There was an error with GET request to comments server");
+      res.sendStatus(500);
+    });
+});
+
+app.post("/comments", (req, res) => {
+  axios
+    .post(commentsRoute + "/comments", req.body)
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log("There was an error with POST request to comments server");
+      res.sendStatus(500);
+    });
+});
+
+////////////////////////////////////////////////
+//////////////// Project Routes ////////////////
+////////////////////////////////////////////////
+
+app.get("/projects", (req, res) => {
+  axios
+    .get(projectsRoute + "/projects", {
+      params: req.query
+    })
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log("There was an error with GET request to projects server");
+      res.sendStatus(500);
+    });
+});
+
+////////////////////////////////////////////////
+//////////////// Pleadge Routes ////////////////
+////////////////////////////////////////////////
+
+app.get("/pledges/:id", (req, res) => {
+  axios
+    .get(pledgesRoute + "/pledges/" + req.params.id)
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log("There was an error with GET request to pledges server");
+      res.sendStatus(500);
+    });
+});
+
+app.post(pledgesRoute + "/pledges", (req, res) => {
+  axios
+    .post("/pledges", req.body)
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log("There was an error with POST request to pledges server");
+      res.sendStatus(500);
+    });
+});
+
+////////////////////////////////////////////////
+//////////////// Related Routes ////////////////
+////////////////////////////////////////////////
+
+app.get("/related", (req, res) => {
+  axios
+    .get(relatedRoute + "/related", {
+      params: req.query
+    })
+    .then(result => {
+      res.status(200);
+      res.json(result.data);
+    })
+    .catch(err => {
+      console.log(
+        "There was an error with GET request to related projects server"
+      );
+      res.sendStatus(500);
+    });
+});
+
+module.exports = app;
